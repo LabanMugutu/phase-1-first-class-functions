24a62b635ab93cf9d20dc45619d19692a176230c
LabanMugutu <labanmugutu@gmail.com>
2025-07-18 21:51:46 +0300

    fixes error

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ function returnsAnamedFunction(){
         return "I am named";
     }
 }
-function returnsAnAnonymousFunction( hook === 'before all'){
-    return function(){
+function returnsAnAnonymousFunction() {
+    return function() {
         return "before all";
-    }
+    };
 }
+
 module.exports ={
     receivesAFunction,
     returnsAnamedFunction,
