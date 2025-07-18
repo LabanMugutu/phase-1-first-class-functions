9cbde0fec38b2bc33a9d1eaa9bfa2080a14a7afa
LabanMugutu <labanmugutu@gmail.com>
2025-07-18 21:56:53 +0300

    fixes error

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-function receivesAFunction(callback){
-    return callback();
+// 1. Function that receives a callback and calls it
+function receivesAFunction(callback) {
+    callback();
 }
-function returnsAnamedFunction(){
-    return function namedFunction(){
-        return "I am named";
-    }
-}
-function returnsAnAnonymousFunction() {
-    return function() {
-        return "before all";
+
+// 2. Function that returns a named function
+function returnsANamedFunction() {
+    return function namedFunction() {
+        return "I am a named function";
     };
 }
 
-module.exports ={
-    receivesAFunction,
-    returnsAnamedFunction,
-    returnsAnAnonymousFunction
+// 3. Function that returns an anonymous function
+function returnsAnAnonymousFunction() {
+    return function () {
+        return "I am an anonymous function";
+    };
 }
+
+// Export them for the tests
+module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+};
